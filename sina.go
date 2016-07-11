b116a09872a6d433b166b1ed329e6852628c91da
hakupoint <hakupoint15@gmail.com>
2016-07-11 13:48:16 +0800

    修改read接口

--- sina.go
+++ sina.go
@@ -3,7 +3,7 @@ package stock
 import (
   "net/http"
   "io/ioutil"
-  "errors"
+  "strings"
 )
 
 const URL = "http://hq.sinajs.cn/list="
@@ -13,7 +13,7 @@ type ThighOrMoney struct{
   money float64
 }
 
-type Sina struct {
+type Stocks struct {
   Name string
   Today float64
   Tomorrow float64
@@ -31,29 +31,44 @@ type Sina struct {
 }
 var indexError error
 
-func Read(num string) (*Sina, error){
-  var sin Sina
-  res,err := http.Get(URL + num)
+func Read(s string) ([]*Stocks, error){
+  list := strings.Split(s, ",")
+  stocksList := []*Stocks{}
+  for _,val := range list{
+    stocks,err := getData(val)
+    if err != nil{
+      return stocksList, err
+    }
+    stocksList = append(stocksList, stocks)
+  }
+  return stocksList, nil
+}
+
+func getData(s string)(*Stocks, error){
+  var stos *Stocks
+  ch := make(chan *Stocks, 0)
+  res,err := http.Get(URL + s)
   if err != nil{
-    return &sin, err
+    return stos, err
   }
 
   defer res.Body.Close()
 
   dst, err := ioutil.ReadAll(res.Body)
   if err != nil{
-    return &sin, err
+    return stos, err
   }
+  go dataProcessing(dst, ch)
+  stos = <-ch
+  return stos, nil
+}
 
-  udst := unicode(string(dst))
+func dataProcessing(s []byte, ch chan *Stocks){
+  udst := unicode(string(s))
   chars := splitString([]rune(udst))
   floats := stringListToFloatList(chars[:], 1, len(chars) - 3)
   chars = append(chars[:1], chars[30:]...)
-  if len(chars) < 3{
-    indexError = errors.New("index out of range")
-    return &sin, indexError
-  }
-  sin = Sina{
+  stocks := Stocks{
       chars[0],
       floats[0],
       floats[1],
@@ -81,8 +96,6 @@ func Read(num string) (*Sina, error){
     chars[1],
     chars[2],
     }
-  return &sin, nil
-
+  ch <- &stocks
 }
-
 
